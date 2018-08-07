dfae2dfe011b9f66a20aaa0e00fecc6e676082a6
Sanjayanbu <snjanbu@gmail.com>
2018-08-07 19:38:55 +0530

    refs #ticket-learning hibernate-retrieval added

--- src/main/dto/UserDto.java
+++ src/main/dto/UserDto.java
@@ -1,18 +1,66 @@
 package main.dto;
 
-import javax.persistence.Column;
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
-@Entity (name="UserDetails")
+@Entity 
+@Table (name="User_Details")
 public class UserDto {
 	
+	/**
+	 * Used to indicate that this will not save in DB
+	 */
+	@Transient
+	private String tenantId;
+	
 	@Id
-	@Column(name="userNo")
 	private int userId;
 	
-	@Column(name="name")
 	private String userName;
+
+	@Lob
+	private String description;
+	
+	@Temporal(TemporalType.DATE)
+	private Date joiningDate;
+	
+	@Temporal(TemporalType.TIME)
+	private Date joiningTime;
+
+	public UserDto() {
+		this.tenantId="sanjay";
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+
+	public Date getJoiningTime() {
+		return joiningTime;
+	}
+
+	public void setJoiningTime(Date joiningTime) {
+		this.joiningTime = joiningTime;
+	}
 
 	public int getUserId() {
 		return userId;
--- src/main/hibernate/HibernateExample.java
+++ src/main/hibernate/HibernateExample.java
@@ -3,6 +3,8 @@
  */
 package main.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -14,17 +16,38 @@ public class HibernateExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		/**
+		 * Setting the UserDto
+		 */
 		UserDto userDto=new UserDto();
 		userDto.setUserId(6101);
 		userDto.setUserName("Santhosh");
+		userDto.setDescription("Sandy");
+		userDto.setJoiningDate(new Date());
+		userDto.setJoiningTime(new Date());
 		
+		/**
+		 * Creating the Session Factory
+		 */
 		SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
+		
+		/**
+		 * Insertion
+		 */
 		Session session=sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(userDto);
 		session.getTransaction().commit();
 		session.close();
 
+		/**
+		 * Retrieval
+		 */
+		userDto=null;
+		session =sessionFactory.openSession();
+		userDto=(UserDto)session.get(UserDto.class, 6101);
+		System.out.println(userDto.getDescription());
+		session.close();
 	}
 
 }
